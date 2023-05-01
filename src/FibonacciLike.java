0fc66ae163097c47c2c26f6685f168410b43e0ea
Gia <gjackson24@scienceleadership.org>
2023-05-01 14:14:05 -0400

    b

--- src/FibonacciLike.java
+++ src/FibonacciLike.java
@@ -3,21 +3,32 @@ public class FibonacciLike implements Fibonacci, Runnable {
     int number1;
     int number2 ;
 
+    String name;
 
-    public FibonacciLike(int amountofNumbersInSequence, int number1, int number2) {
+
+    public FibonacciLike(String name, int amountofNumbersInSequence, int number1, int number2) {
+       this.name = name;
         this.amountofNumbersInSequence = amountofNumbersInSequence;
         this.number1 = number1;
         this.number2 = number2;
     }
 
     public void run(){
-        for(int Num = number1; Num < amountofNumbersInSequence; Num++){
+        System.out.println(name+ number1);
+        System.out.println(name+ number2);
+
+        for(int Num = 0; Num < amountofNumbersInSequence - 2; Num++){
             int num3 = number1 + number2;
-            System.out.println(num3);
+            System.out.println(name + num3);
             number1 = number2;
             number2 = num3;
+            try {
+                Thread.currentThread().sleep(0);
+            } catch (Exception ex) {
+                Thread.currentThread().interrupt();
+            }
 
-            //System.out.println(Num);
         }
+        //System.out.println("Done");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        boolean useManyThreads = true;
+
         System.out.println("Hello world!");
-        FibonacciLike WEEEEEE = new FibonacciLike(10,0,1);
-        WEEEEEE.run();
+        FibonacciLike WEEEEEE = new FibonacciLike("Sequence 1: ", 5, 0, 1);
+        FibonacciLike WEEEEEE2 = new FibonacciLike("Sequence 2: ",5, 2, 7);
+
+        if (useManyThreads) {
+            Thread  NumLaunchingThread = new Thread(WEEEEEE);
+            Thread Num2LaunchingThread = new Thread(WEEEEEE2);
+            NumLaunchingThread.start();
+            Num2LaunchingThread.start();
+        } else {
+            WEEEEEE.run();
+            WEEEEEE2.run();
+        }
     }
 }
